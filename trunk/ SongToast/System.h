266cf4ea9633d14871be527b4152b9ca08234458
instcode <instcode@3660ff36-4840-0410-ab72-675ca585a781>
2007-12-02 07:07:42 +0000

    Initial version

---
+++ trunk/ SongToast/System.h
@@ -0,0 +1,10 @@
+#ifndef _THREAD_H_
+#define _THREAD_H_
+
+double GetCPUUsages();
+BOOL BeginThread(LPTHREAD_START_ROUTINE lpStartAddress, LPVOID lpParameter);
+BOOL WaitForThreadsToExit();
+void ReadProcess(DWORD dwProcessId, LPCVOID lpBaseAddr, LPSTR lpszBuffer, IN OUT DWORD& dwSize);
+void SysTrace(const char *szFormat, ...);
+
+#endif
---
+++ trunk/ SongToast/resource.h
@@ -0,0 +1,27 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by SongToast.rc
+//
+#define IDB_SKIN_BOARD                  2
+#define IDC_TEXT_TOAST_UPDATE_TIME      2000
+#define IDD_DIALOG1                     2001
+#define IDD_CONFIG_DIALOG               2001
+#define IDC_CHECK_SHOW_TOAST_WINDOW     2002
+#define IDC_EDIT_TOAST_TIME_TO_STAY     2003
+#define IDC_ADD_VARIABLES               2004
+#define IDC_CHECK_YM_STATUS_MESSAGE     2005
+#define IDC_EDIT_TEMPLATE               2006
+#define IDC_EDIT_YID                    2007
+#define IDC_EDIT5                       2008
+#define IDC_EDIT_STATUS_UPDATE_TIME     2008
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        2002
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         2005
+#define _APS_NEXT_SYMED_VALUE           2000
+#endif
+#endif
